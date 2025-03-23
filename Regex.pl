9d77251e1b174ff27dcb9043c1236afba3a3752b
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2025-03-22 18:41:15 -0600

    Update Regex.pl

--- Regex.pl
+++ Regex.pl
@@ -1,13 +1,29 @@
-:- use_module(library(pcre)). 
+:- use_module(library(pcre)).
 
 % Expresión regular equivalente al autómata
-expresion_regular("^d(ina$|o(r|l)$|r(aug|ego))"). 
-
-% 1er caso: "Debe empezar con d y terminar con ina" dina 
-% 2do caso : "Debe empezar con d debe tener una o y terminar con o u l" dol y dor 
-% 3er caso : "Debe empezar con d debe tener un r y puede terminar en aug o en ego"  drego draug
+expresion_regular("^d(ina$|o(r|l)$|r(aug|ego))").
 
 % Predicado que verifica si una palabra es aceptada
 acepta_regex(Palabra) :-
     expresion_regular(Regex),   % Obtener la expresión regular
     re_match(Regex, Palabra, []).  % Verificar si la palabra coincide
+
+% Predicado para realizar el benchmark
+benchmark(Palabra) :-
+    % Medir el tiempo antes de la ejecución
+    statistics(runtime, [StartTime, _]),
+    
+    % Ejecutar el predicado acepta_regex con la palabra
+    acepta_regex(Palabra),
+
+    % Medir el tiempo después de la ejecución
+    statistics(runtime, [EndTime, _]),
+    
+    % Calcular el tiempo transcurrido
+    Runtime is EndTime - StartTime,
+
+     % Convertir el tiempo a milisegundos
+    Runtime is Runtime / 1000000,
+    
+    % Mostrar el tiempo de ejecución
+    write('Tiempo de ejecución para "'), write(Palabra), write('": '), write(Runtime), nl.
